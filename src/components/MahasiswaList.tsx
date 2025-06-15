ac3dcef22f4b5f4816d13f588b9a22610f8144d7
Sastraaaa <kharismasastrawansyah@gmail.com>
2025-06-15 19:33:36 +0700

    refactor(components): move mahasiswa data to separate module
    - Remove inline dummy data from MahasiswaList component
    - Import mahasiswa data from new dedicated data module
    - Improves code organization and separation of concerns
    - Makes data more reusable across different components

--- src/components/MahasiswaList.tsx
+++ src/components/MahasiswaList.tsx
@@ -1,28 +1,5 @@
+import { someMahasiswa } from "../data/mahasiswa";
 import ProfileCard from "./ProfileCard";
-
-type Mahasiswa = {
-  name: string;
-  majority?: string;
-  birth: number;
-};
-
-// Data Dummy
-const someMahasiswa: Mahasiswa[] = [
-  {
-    name: "Mamat Karbit",
-    majority: "Teknik Lele",
-    birth: 2000,
-  },
-  {
-    name: "Mamat Uncal",
-    majority: "Teknik informatika",
-    birth: 2000,
-  },
-  {
-    name: "Mamat Uncal",
-    birth: 2000,
-  },
-];
 
 const MahasiswaList = () => {
   return (
