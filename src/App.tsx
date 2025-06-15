549ace7ab8490d753f08aa22a05f62b03c31b603
Sastraaaa <kharismasastrawansyah@gmail.com>
2025-06-15 19:25:06 +0700

    refactor(App): replace inline mahasiswa rendering with MahasiswaList
    - Remove ProfileCard component and mahasiswa type definition from App.tsx
    - Delete inline mapping of dummy data in favor of dedicated MahasiswaList component
    - Clean up App component by moving mahasiswa-related logic to separate component
    - Maintain all existing functionality while improving code organization

--- src/App.tsx
+++ src/App.tsx
@@ -2,43 +2,13 @@ import "./App.css";
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import LikeCounter from "./components/LikeCounter";
-import ProfileCard from "./components/ProfileCard";
-
-type Mahasiswa = {
-  name: string;
-  majority?: string;
-  birth: number;
-};
-
-// Dummy Data
-const someMahasiswa: Mahasiswa[] = [
-  {
-    name: "Mamat Karbit",
-    majority: "Teknik Lele",
-    birth: 2000,
-  },
-  {
-    name: "Mamat Uncal",
-    majority: "Teknik informatika",
-    birth: 2000,
-  },
-  {
-    name: "Mamat Uncal",
-    birth: 2000,
-  },
-];
+import MahasiswaList from "./components/MahasiswaList";
 
 function App() {
   return (
     <>
       <Header title="Data Mahasiswa" />
-
-      {someMahasiswa.map((mahasiswa, id) => {
-        const { name, majority, birth } = mahasiswa;
-        return (
-          <ProfileCard key={id} name={name} majority={majority} birth={birth} />
-        );
-      })}
+      <MahasiswaList />
       <Header title="Counter Apps" />
       <Counter />
       <Header title="Like and Dislike" />
