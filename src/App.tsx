3d06800cade93afc0cf97c22f5bb919c961e11ae
Sastraaaa <kharismasastrawansyah@gmail.com>
2025-06-16 21:21:50 +0700

    feat(routing): add react-router configuration with basic pages
    - Import Route and Routes from react-router-dom
    - Add HomePage, ProductPage and NotFoundPage imports
    - Set up routing with paths for home, product and 404 scenarios
    - Implement dynamic routing for product slugs
    - Maintain existing components alongside new routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import LikeCounter from "./components/LikeCounter";
 import MahasiswaList from "./components/MahasiswaList";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
@@ -13,6 +17,13 @@ function App() {
       <Counter />
       <Header title="Like and Dislike" />
       <LikeCounter />
+
+      <Routes>
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/product/:slug" element={<ProductPage />} />
+        <Route path="/product/" element={<ProductPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
     </>
   );
 }
