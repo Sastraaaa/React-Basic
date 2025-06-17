4978a8b36f97a94e0670a8e2b9d1d949f9659dce
Sastraaaa <kharismasastrawansyah@gmail.com>
2025-06-17 17:28:14 +0700

    refactor(routing): restructure app layout and routing configuration
    - Remove deprecated components (Counter, Header, LikeCounter, MahasiswaList)
    - Add new Dashboard component as default route ("/")
    - Reorganize route structure with proper ordering
    - Clean up unused imports and empty comments
    - Improve routing clarity with explicit paths before wildcard route

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,20 @@
 import { Route, Routes } from "react-router";
 import "./App.css";
-import Counter from "./components/Counter";
-import Header from "./components/Header";
-import LikeCounter from "./components/LikeCounter";
-import MahasiswaList from "./components/MahasiswaList";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <>
-      <Header title="Data Mahasiswa" />
-      <MahasiswaList />
-      <Header title="Counter Apps" />
-      <Counter />
-      <Header title="Like and Dislike" />
-      <LikeCounter />
-
       <Routes>
+        <Route path="/" element={<Dashboard />} />
         <Route path="/home" element={<HomePage />} />
 
         {/* Dynamic Path */}
         <Route path="/product/:slug" element={<ProductPage />} />
+        {/*  */}
 
         <Route path="/product/" element={<ProductPage />} />
         <Route path="*" element={<NotFoundPage />} />
